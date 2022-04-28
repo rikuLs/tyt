e1cb4ff7abee70a06824de3cb113e22dbceb1e92
rikuLs <104224699+rikuLs@users.noreply.github.com>
2022-04-28 12:20:46 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+
+
+var prefix = "s";
+
+const ownerID = '608267144878096404';
+
+
+
+
+
+
+
+
+client.login(process.env.t1);// 
+client2.login(process.env.t2);// 
+client3.login(process.env.t3);// 
+client4.login(process.env.t4);// 
+client5.login(process.env.t5);// 
+client6.login(process.env.t6);// 
+client7.login(process.env.t7);// 
+client8.login(process.env.t8);// 
+client9.login(process.env.t9);// 
+client10.login(process.env.t10);// 
